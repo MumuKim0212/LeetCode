508e6d4e6f20633de7c2bb6d5ee97115e560a95c
Gyeongmin Kim <167409133+MumuKim0212@users.noreply.github.com>
2025-04-02 16:51:33 +0900

    Update 1071-greatest-common-divisor-of-strings.cpp

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,3 +1,44 @@
+/* 정답
+아이디어:
+두 문자열이 같은 패턴이라면 두 문자열을 합쳤을 때 순서와 상관 없이 동일해야함
+동일한 문자열이라면 같은 패턴을 갖고 있음
+이때 두 문자열 길이의 최대공약수가 패턴
+
+문제:
+
+해결:
+
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        // 두 문자열이 같은 패턴으로 구성되어 있는지 확인
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        // 두 문자열 길이의 최대공약수 계산
+        int gcdLength = gcd(str1.length(), str2.length());
+
+        // 최대공약수 길이만큼의 접두사 반환
+        return str1.substr(0, gcdLength);
+    }
+
+    // 유클리드 알고리즘으로 최대공약수 계산
+    int gcd(int a, int b) {
+        while (b) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
+
 /*
 아이디어:
 만약 나눠진다면 문자열의 길이는 패턴의 배수
@@ -8,6 +49,7 @@
 해결:
 
 */
+/*
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -51,3 +93,4 @@ public:
         return "";
     }
 };
+*/
